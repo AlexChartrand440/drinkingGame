5eb4f302f49d699d5f30b3aa7ebafb566edc910d
unknown <jules.grateau@gmail.com>
2019-05-25 11:45:02 +0100

    Clean des composants de template non utilisés

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,9 +6,6 @@ import {
 } from "react-navigation";
 
 import TabBarIcon from "../components/TabBarIcon";
-import HomeScreen from "../screens/HomeScreen";
-import LinksScreen from "../screens/LinksScreen";
-import SettingsScreen from "../screens/SettingsScreen";
 import PlayerSelectionContainer from "../screens/PlayerSelection/PlayerSelectionContainer";
 
 const PlayerSelectionStack = createStackNavigator({ PlayerSelectionContainer });
@@ -23,55 +20,6 @@ PlayerSelectionStack.navigationOptions = {
   )
 };
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen
-});
-
-HomeStack.navigationOptions = {
-  tabBarLabel: "Home",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "md-information-circle"
-      }
-    />
-  )
-};
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: "Links",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === "ios" ? "ios-link" : "md-link"}
-    />
-  )
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: "Settings",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
-    />
-  )
-};
-
 export default createBottomTabNavigator({
-  PlayerSelectionStack,
-  HomeStack,
-  LinksStack,
-  SettingsStack
+  PlayerSelectionStack
 });
